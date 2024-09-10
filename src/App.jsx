768b1b87312eab05883578fccc2f5feb4afbb5af
Breiche32 <sinal_breish@outlook.com>
2024-09-11 01:55:16 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import React from "react";
-
 import {
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements,
-  Router,
-  RouterProvider,
+  HashRouter,
+  Routes,
+  Route
 } from "react-router-dom";
 
 import HomePage from "./Pages/HomePage";
@@ -13,21 +10,17 @@ import MainLayout from "./Layout/MainLayout";
 import Projects from "./Components/Projects/Projects";
 import SoftwareCategories from "./Components/SoftwareCategories/SoftwareCategories";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
-      <Route index element={<HomePage />} />
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/softwareCategories" element={<SoftwareCategories />} />
-    </Route>
-  )
-);
-
 function App() {
   return (
-    <>
-      <RouterProvider router={router} />
-    </>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="softwareCategories" element={<SoftwareCategories />} />
+        </Route>
+      </Routes>
+    </HashRouter>
   );
 }
 
